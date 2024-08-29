78b220657f86ebdd9441d52da8c539a0bc1002b2
gollasreenu <vasu22042001@gmail.com>
2024-08-29 20:38:07 +0530

    Update routing to use HashRouter and configure routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
 
-import SignUp from './Components/SignUp'
-import SignIn from './Components/SignIn'
-import MainPage from './Components/MainPage'
-import UserPage from './Components/UserPage'
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
+import MainPage from './Components/MainPage';
+import UserPage from './Components/UserPage';
 
 const App = () => {
-
   return (
     <Router>
-    <Routes>
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/mainPage" element={<MainPage />} />
-      <Route path="/user/:userId" element={<UserPage />} />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/user/:userId" element={<UserPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
