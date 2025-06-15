a167125fb9e711ecf4fbc012de9ed20f189b99ee
gollasreenu <vasu22042001@gmail.com>
2025-06-15 12:36:35 +0530

    Fix router basename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import React from 'react';
 
 import SignUp from './Components/SignUp';
